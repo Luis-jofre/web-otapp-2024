5fbffddd2a08dbf6ad60eae9e51c8133e1e05944
Luis Jofré <krox.mc@gmail.com>
2024-07-13 11:36:39 -0400

    avance footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -5,25 +5,31 @@ import logo from '../img/logo.jpg';
 const Footer = () => {
 
     return (
-        <footer className="footer">
-            <div className="columnLarge">
-            <div classNagoContainerme="logo-footer-container">
-                <Image src={logo} alt="Logo-footer"/>
-             </div>
-            </div>
+        <footer>
+            <div className="footer">
+                <div className="cont-footer">
+                <div className="columnLarge">
+                <div classNagoContainerme="logo-footer-container">
+                    <Image src={logo} alt="Logo-footer"/>
+                </div>
+                </div>
                 <div className="columnSmall">
-                <h2>Soporte</h2>
-                <ul>
-                    <li>Preguntas frecuentes </li>
-                    <li>Documentación </li>
-                </ul>
-            </div>
+                    <h2>Soporte</h2>
+                    <ul>
+                        <li>Preguntas frecuentes </li>
+                        <li>Documentación </li>
+                    </ul>
+                </div>
                 <div className="columnSmall">
-                <h2>Descarga en</h2>
-                <button>1</button>
-                <button>2</button>
+                    <h2>Descarga en</h2>
+                    <button>1</button>
+                    <button>2</button>
+                </div>
+                </div>
             </div>
+                 <div className="firm">Diseño by Propulsa</div>
       </footer>
+         
      
       );
         
