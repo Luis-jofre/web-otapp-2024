7bec93500ee47929d54e471a9750477aa5f3f90a
Luis Jofré <krox.mc@gmail.com>
2024-07-14 12:17:45 -0400

    actualización propuesta de valor

--- src/app/propuestadevalor.js
+++ src/app/propuestadevalor.js
@@ -1,24 +1,30 @@
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+
 const Propuestadevalor = () => {
 
     return (
        <div className="flex min-h-screen flex-col items-center justify-between p-24">
         <div class="content">
+       
         <h2>Por qué tu empresa debería utilizar Otapp</h2>
            <div class="contentValor">
              <div className="propuestaValor">
-               <div class="titleValor">Formularios personalizados</div>
+             <FaRegArrowAltCircleRight style={{ marginRight: '10px', fontSize: '2rem', color:'#0DB3CF' }} />
+               <h3>Formularios personalizados</h3>
 
                <p>2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación</p>
              </div>
 
              <div className="propuestaValor">
-               <div class="titleValor">Formularios personalizados</div>
+             <FaRegArrowAltCircleRight style={{ marginRight: '10px', fontSize: '2rem', color:'#0DB3CF' }} />
+               <h3>Formularios personalizados</h3>
 
                <p>2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación</p>
              </div>
 
              <div className="propuestaValor">
-               <div class="titleValor">Formularios personalizados</div>
+             <FaRegArrowAltCircleRight style={{ marginRight: '10px', fontSize: '2rem', color:'#0DB3CF' }} />
+               <h3>Formularios personalizados</h3>
 
                <p>2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación2 este es el primer modulo de propuesta de valor, aquí se hablará del principal beneficio de la aplicación</p>
              </div>
