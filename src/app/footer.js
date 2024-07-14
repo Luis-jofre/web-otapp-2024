0c1c63e1d21263a0b4d91e5fb7c6e90a8fbf63da
Luis Jofré <krox.mc@gmail.com>
2024-07-14 10:55:27 -0400

    diseño casi terminado completo de footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,6 +1,8 @@
 import './globals.css'; 
 import Image from 'next/image';
-import logo from '../img/logo.jpg'; 
+import FirmPropulsa from '../img/firm-propulsa.webp'; 
+import logoOtappBlanco from '../img/logo-otapp-blanco.svg';
+import { FaApple, FaGooglePlay, FaWhatsapp, FaLinkedinIn, FaInstagram, FaFacebook } from 'react-icons/fa'; 
 
 const Footer = () => {
 
@@ -9,8 +11,8 @@ const Footer = () => {
             <div className="footer">
                 <div className="cont-footer">
                 <div className="columnLarge">
-                <div classNagoContainerme="logo-footer-container">
-                    <Image src={logo} alt="Logo-footer"/>
+                <div classNago="logo-footer-container">
+                    <Image src={logoOtappBlanco}  className="Logo-footer" alt="Logo-footer"/>
                 </div>
                 </div>
                 <div className="columnSmall">
@@ -21,13 +23,49 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className="columnSmall">
-                    <h2>Descarga en</h2>
-                    <button>1</button>
-                    <button>2</button>
+                    <h2>Síguenos</h2>
+                    <div className="social">
+                            <a href='https://api.whatsapp.com/send/?phone=56938825343&text=%C2%A1Hola%21&type=phone_number&app_absent=0'>
+                            <FaWhatsapp style={{ marginRight: '10px', fontSize: '1.5rem' }} />
+                            </a>
+                            <a href='https://www.linkedin.com/company/81847489/admin/dashboard/'>
+                            <FaLinkedinIn style={{ marginRight: '10px', fontSize: '1.5rem' }} />
+                            </a>
+                            <a href='https://www.instagram.com/otapp.cl/'>
+                            <FaInstagram style={{ marginRight: '10px', fontSize: '1.5rem' }} />
+                            </a>
+                            <a href='#'>
+                            <FaFacebook style={{ marginRight: '10px', fontSize: '1.5rem' }} />
+                            </a>
+                      
+                    </div>
                 </div>
+
+                <div className="columnSmall">
+                    <h2>Descarga en</h2>
+                    <a href='https://apps.apple.com/us/app/otapp/id6445826287'>
+                        <button> 
+                            <FaApple style={{ marginRight: '10px', fontSize: '2rem' }} />
+                            <div class="text-button">
+                            <h4>Descargala en</h4> 
+                            <h3>App Store</h3>
+                            </div>
+                        </button>
+                    </a>
+
+                    <a href='https://play.google.com/apps/internaltest/4701154468789251585'>
+                    <button>
+                    <FaGooglePlay style={{ marginRight: '10px', fontSize: '2rem' }} />
+                        <div class="text-button">
+                        <h4>Descargala en</h4> 
+                        <h3>Google Play</h3>
+                        </div>
+                    </button>
+                    </a>
                 </div>
             </div>
-                 <div className="firm">Diseño by Propulsa</div>
+        </div>
+                 <div className="firm">Diseño by Propulsa   <Image src={FirmPropulsa} /></div>
       </footer>
          
      
